a596e570a8370141ab59bbf49dabcad12789625b
Enjoy <278571407@qq.com>
2021-09-09 18:25:24 +0800

    9.9

--- main/alien_invasion.py
+++ main/alien_invasion.py
@@ -5,7 +5,7 @@ class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self):
         """初始化游戏并创建游戏资源"""
-        paygame.init()
+        pygame.init()
 
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
